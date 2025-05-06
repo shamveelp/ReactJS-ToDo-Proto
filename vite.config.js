4078bff4cebe3efe7a421de5fd49a85d4c9aa7e7
Shamveel P <shamveelp007@gmail.com>
2025-05-07 00:01:48 +0530

    Proto Version 001 edit 001

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/ReactJS-ToDo-Proto/', // 👈 use your GitHub repo name with slashes
   plugins: [react()],
 })
